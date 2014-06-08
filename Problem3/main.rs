e5c1fe83516d8c0be24d83d26185aa9faa696a1b
Grant Mercer <Gmercer015@gmail.com>
2014-06-08 12:18:14 -0500

    updated problem 3 source code

--- Problem3/main.rs
+++ Problem3/main.rs
@@ -1,6 +1,6 @@
 
 
-fn find_factors(mut n: u64,mut fctrs: Vec<u64>) {
+fn find_factors(mut n: u64, fctrs: &mut Vec<u64>) {
 	let mut z: u64 = 2;
 
 	while z*z <= n {
@@ -12,19 +12,18 @@ fn find_factors(mut n: u64,mut fctrs: Vec<u64>) {
 	if n > 1 {
 		fctrs.push(n);
 	}
-
-	
-	let size = fctrs.len();
-	for i in range(0, size) {
-		let value = fctrs.get(i);
-		println!("{}", value);
-	}
-	
 }
 
 fn main() {
-	let mut primeFactors: Vec<u64> = Vec::new();
+	let mut prime_factors: Vec<u64> = Vec::new();
 	let bigNum: u64 = 600851475143;
-	find_factors(bigNum, primeFactors);
+	find_factors(bigNum, &mut prime_factors);
+	
+	let size = prime_factors.len();
+	for i in range(0, size) {
+		let value = prime_factors.get(i);
+		println!("{}", value);
+	}
+
 }
 
