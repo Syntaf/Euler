5695a1ebca2d7f80723b00abf57f062f93257f49
Grant Mercer <Gmercer015@gmail.com>
2014-08-03 14:35:00 +0000

    started work on problem10

---
+++ Problem10/main.rs
@@ -0,0 +1,30 @@
+/*
+    Find the sum of all the primes below two million.
+*/
+fn is_prime(num: i64) -> bool {
+  if num <= 1 as i64 {
+    false
+  } else if num == 2i64 {
+    true
+  } else if num % 2i64 == 0i64 {
+    false
+  } else {
+    let mut j = 3i64;
+    let mut prime = true;
+    loop {
+      if j*j > num {
+        break;
+      }
+      if num % j == 0 {
+        prime = false;
+        break;
+      }
+      j += 1;
+    }
+    prime
+  }
+}
+
+fn main() {
+  println!("{}",is_prime(7));
+}
